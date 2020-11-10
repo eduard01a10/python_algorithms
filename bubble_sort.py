2ecce19bf7ff65a644bc03abfe7ae413aea983c9
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-11-09 19:42:39 -0600

    Fixed some comments

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,18 +5,21 @@
 import random
 
 def bubble_sort(list_numbers):
+    # Define the lenght of the array it's necessary because the algorithms work with indexes
     n = len(list_numbers)
     
-    ## First for loop which takes the index of the set of numbers
+    ## First for loop which takes the index of the set of numbers this will go through all the index
     for i in range(n):
 
         ## Second for loop which it wil reduce the last number of the set of numbers and start again
+        ## j is the second iterator this will go from 0 to n (the las index of the array) minus i minus 1 
+        ## in order to acces to the last index of the array
         for j in range(0, n - i - 1):
 
-            ## This is the swaping, it wil replace the higher number to the left
+            ## This if statement compares the number with the other at the right
             if list_numbers[j] > list_numbers[j + 1]:
 
-                ## This line of code represents the number j equals to j + 1 and j + 1 equal to j
+                ## This line makes the swaping
                 list_numbers[j], list_numbers[j + 1] = list_numbers[j + 1], list_numbers[j]
     
     return list_numbers 
